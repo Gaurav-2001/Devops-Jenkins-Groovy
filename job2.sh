3bfe2c0df947e092c8c3d9079f1b045c193cbbc2
Gaurav-2001 <64505832+Gaurav-2001@users.noreply.github.com>
2020-08-03 05:16:57 +0530

    first

---
+++ job2.sh
@@ -0,0 +1,9 @@
+p=$(kubectl get service/httpd -o jsonpath="{.spec.ports[0].nodePort}")
+exit_code=$(curl -o /dev/null -s -w "%{http_code}" http://192.168.99.100:$p)
+if [ $exit_code == 200 ]
+then
+exit 0
+else
+python3 mail.py
+exit 1
+fi
