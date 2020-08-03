732fa1a9a12a693f509a444c3340876d5c728a2c
Gaurav-2001 <64505832+Gaurav-2001@users.noreply.github.com>
2020-08-03 06:09:45 +0530

    second

--- job1.sh
+++ job1.sh
@@ -2,7 +2,7 @@ if kubectl get deployment | grep httpd
 then
 echo "server is already running"
 else
-kubectl create -f httpd_dep1.yml
+kubectl create -f /task6_code/httpd_dep1.yml
 sleep 30
 POD=$(kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")
 kubectl cp /task6_code/*.html $POD:/usr/local/apache2/htdocs/
