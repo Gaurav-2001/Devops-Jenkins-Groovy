f2c0a50488b294af10054c6a26e41cbc1d336251
Gaurav-2001 <64505832+Gaurav-2001@users.noreply.github.com>
2020-08-03 06:43:55 +0530

    first

---
+++ groovy.groovy
@@ -0,0 +1,26 @@
+job('job1-create-web-deployment') {
+    steps {
+        shell('bash /task6_code/job1.sh')
+    }
+}
+job('job2-test-and-mail-to-developer-when-fail') {
+    triggers {
+        upstream {
+            upstreamProjects('job1-create-web-deployment')
+            threshold('SUCCESS')
+        }
+    }
+    steps {
+        shell('bash /task6_code/job2.sh')
+    }
+}
+buildPipelineView('devops-task-6') {
+    filterBuildQueue()
+    filterExecutors()
+    title('Task-6-using groovy-code')
+    displayedBuilds(5)
+    selectedJob('job1-create-web-deployment')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
+}
